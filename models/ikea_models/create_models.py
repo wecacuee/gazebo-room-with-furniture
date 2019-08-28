06a29320bf83c822a8f7b49c5fddefa3171d42a5
Vikas Dhiman <wecacuee@users.noreply.github.com>
2019-08-27 23:44:00 -0700

    try to guess inertia too

--- models/ikea_models/create_models.py
+++ models/ikea_models/create_models.py
@@ -39,29 +39,39 @@ def mesh_shift_scale_normalize_max(objfile, maxdim=1):
     mins = np.quantile(points, 0.05, axis=0)#  points.min(axis=0)
     maxs = np.quantile(points, 0.95, axis=0)# points.max(axis=0)
     # center = tvtk_get_center_of_mass(polydata)
-    center = (maxs + mins)/2
-    scale = np.ones(3) * (maxs - mins).max()
-    Roll_90 = np.array([[1.0,  0.0, 0.0],
-                        [0.0,  0.0, 1.0],
-                        [0.0, -1.0, 0.0]])
-    return Roll_90.dot(- center / scale).tolist(), (1 / scale).tolist()
+    dims = (maxs - mins)
+    mesh_maxdim = dims.max()
+    rescale_factor = np.ones(3) * maxdim / mesh_maxdim
+    rescaled_dims = dims * rescale_factor
+    return rescaled_dims.tolist(), rescale_factor.tolist()
 
 
 def main(topdir="ikea_models/meshes",
          template_fmt="ikea_models/{file}.erb",
-         generated_files=["model.config", "model.sdf"]):
+         generated_files=["model.config", "model.sdf"],
+         desired_max_dim=dict(
+             bed=3,
+             bookcase=3,
+             chair=1,
+             desk=1.5,
+             table=1.5,
+             sofa=2.0,
+             wardrobe=3)):
     for objfile in find_files(topdir):
-        objcenter, objscale = mesh_shift_scale_normalize_max(objfile)
         objfileparts = objfile.split(osp.sep)
         ikea_names = [fp for fp in objfileparts if fp.startswith("IKEA_")]
+        _, ikea_type, *ikea_subtype = ikea_names[0].split("_")
         ikea_model_dir = osp.join("ikea_models", ikea_names[0])
+        objdims, objscale = mesh_shift_scale_normalize_max(
+            objfile,
+            maxdim=desired_max_dim[ikea_type])
         Path(ikea_model_dir).mkdir(parents=True, exist_ok=True)
         for f in generated_files:
             p = subprocess.Popen(["erb", template_fmt.format(file=f)],
                                  stdout=open(osp.join(ikea_model_dir, f), "w"),
                                  env=dict(IKEA_OBJ_FILENAME=objfile,
                                           IKEA_OBJ_SCALE=dump(objscale),
-                                          IKEA_OBJ_CENTER=dump(objcenter)))
+                                          IKEA_OBJ_DIMS=dump(objdims)))
             p.wait(timeout=30)
 
 
