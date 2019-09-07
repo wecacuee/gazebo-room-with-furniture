64b56d829770545b6769d9458980c0190d002b22
Vikas Dhiman <wecacuee@users.noreply.github.com>
2019-09-06 21:20:44 -0700

    we have better inertia and center of gravity to be able to afford non-static objects

--- setup.bash
+++ setup.bash
@@ -4,13 +4,13 @@ function setup_env() {
     local setupdir="$(cd "$scriptdir" && pwd)"
 
     {
-        source /usr/share/gazebo-9/setup.sh
+        [ -f /usr/share/gazebo-9/setup.sh ] && source /usr/share/gazebo-9/setup.sh
         source $setupdir/gazebo_models/setup.bash
         unset GAZEBO_MODEL_URI
     }
 
     export GAZEBO_MODEL_PATH="$setupdir/models:$GAZEBO_MODEL_PATH"
-    source envrc
+    [ -f $setupdir/envrc ] && source $setupdir/envrc
     if [ -z "$IKEA_MODELS_DATASET_DIR" ]; then
         echo "Please download set IKEA_MODELS_DATASET_DIR from http://ikea.csail.mit.edu/zip/IKEA_models.zip"
     fi
